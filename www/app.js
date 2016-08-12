587783b1413fb8f25e90ac385c6c41959c09673f
Shehzad Noor Taus Priyo <sntaus@gmail.com>
2016-08-11 20:19:53 -0700

    Added code to show points on map

--- www/app.js
+++ www/app.js
@@ -3,6 +3,32 @@
   // Our Google map.
   var map;
 
+  var deg2rad = function(angle) {
+    return (angle / 180) * Math.PI;
+  }
+
+  var rad2deg = function(rad) {
+    return (rad * 180) / Math.PI;
+  }
+
+  // calculate destination lat/lon given a starting point, bearing, and distance (in m)
+  var destination = function(lat, lon, bearing, distance) {
+    var radius = 6378137;
+    var rLat = deg2rad(lat);
+    var rLon = deg2rad(lon);
+    var rBearing = deg2rad(bearing);
+    var rAngDist = distance / radius;
+
+    rLatB = Math.asin((Math.sin(rLat) * Math.cos(rAngDist)) + (Math.cos(rLat) * Math.sin(rAngDist) * Math.cos(rBearing)));
+
+    rLonB = rLon + Math.atan2(Math.sin(rBearing) * Math.sin(rAngDist) * Math.cos(rLat), Math.cos(rAngDist) - Math.sin(rLat) * Math.sin(rLatB));
+
+    return {
+      "lat": rad2deg(rLatB), 
+      "lng": rad2deg(rLonB)
+    };
+  }
+
   var flightPath;
   var image;
   var updateElevationsDistance = function(els) {
@@ -34,9 +60,11 @@
     var elevations = [null, null];
     var state = 0;
     var point1, point2;
+    var rectangle;
 
     map.addListener('click', function(coords) {
       var point = ee.Geometry.Point(coords.latLng.lng(), coords.latLng.lat());
+
       var meanDictionary = image.reduceRegion(ee.Reducer.mean(), point, 30);
       var statement = meanDictionary.get("elevation");
       if (state == 0) {
@@ -49,6 +77,44 @@
       } else {
         point2 = point;
         point1.distance(point2).evaluate(function(val) {
+          var positionObj;
+          var interval = val / 35;
+
+          var apart;
+
+          var pointRadius = val/70;
+
+          for(var distance = val; distance > 0; distance -= interval) {
+            var circumference = 2 * Math.PI * distance;
+            var numberOfIterations = Math.round(circumference / (pointRadius * 2))
+
+            apart = 360 / numberOfIterations;
+            for(var deg = 0; deg < 359; deg += apart) {
+
+              var positionObj = destination(point1.coordinates_[1], point1.coordinates_[0], deg, distance);
+
+              var visualization = new google.maps.Circle({
+                strokeOpacity: 1,
+                strokeWeight: 0,
+                fillColor: 'rgb(255, 100, 0)',
+                fillOpacity: 0.85,
+                center: positionObj,
+                radius: pointRadius
+              });
+
+              var enclosingObject = {
+                position: positionObj,
+                visualization: visualization
+              }
+
+              visualization.setMap(map);
+            }
+          }
+          
+          
+
+
+
           $("#distance").val(Math.round(val * 1000) / 1000);
         });
 
@@ -61,7 +127,32 @@
           });
           flightPath.setMap(map);
         }
+        var regionGeo = ee.Geometry.Rectangle([point1.coordinates_[0], point1.coordinates_[1], point2.coordinates_[0], point2.coordinates_[1]]);
+        image.reduceRegion(ee.Reducer.toList(), regionGeo, 30).evaluate(function(val){
+          console.log("toList output -");
+          console.log(val);
+          console.log("-");
+        });
+        /*
+        if(rectangle == null) {
+          rectangle = new google.maps.Rectangle({
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.35
+          });
+        }
 
+        rectangle.setBounds({
+          north: Math.max(point2.coordinates_[1], point1.coordinates_[1]),
+          south: Math.min(point1.coordinates_[1], point2.coordinates_[1]),
+          east: Math.max(point2.coordinates_[0], point1.coordinates_[0]),
+          west: Math.min(point2.coordinates_[0], point1.coordinates_[0])
+        });
+
+        rectangle.setMap(map);
+        */
         var flightPlanCoordinates = [
           {lat: point1.coordinates_[1], lng: point1.coordinates_[0]},
           {lat: point2.coordinates_[1], lng: point2.coordinates_[0]}
@@ -89,8 +180,9 @@
   $(document).ready(function() {
     // Create the base Google Map.
     map = new google.maps.Map($('.map').get(0), {
-          center: { lat: 49.2827, lng: -123.1207},
-          zoom: 5
+          center: { lat: 49.40755, lng: -122.93791},
+          zoom: 15,
+          mapTypeId: 'satellite'
         });
 
     $("#calc_button").click(function(){
